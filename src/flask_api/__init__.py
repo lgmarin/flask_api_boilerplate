15580c377822d534f913ee5eba8a9b7abc789eaa
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-19 14:08:28 -0300

    Main App Factory

--- src/flask_api/__init__.py
+++ src/flask_api/__init__.py
@@ -1,14 +1,27 @@
+""" flask_api Main Application Factory
+    Create the Flask App and binds all the extensions to it
+"""
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from flask_api.config import get_config
 
 cors = CORS()
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
 
 def create_app(config_name):
     app = Flask("flask_api")
     app.config.from_object(get_config(config_name))
+
+    cors.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
     return app
