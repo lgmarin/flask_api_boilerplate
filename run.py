0c780a1e189bd900f36983932ab2fe07aa72396f
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-25 10:52:31 -0300

    Create model entry in the app shell

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
 
 from flask_api_boilerplate import create_app, db
+from flask_api_boilerplate.models.user import User
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
 
 @app.shell_context_processor
 def shell():
-    return {"db": db}
+    return {"db": db, "User": User}
