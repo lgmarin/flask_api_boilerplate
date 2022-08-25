75da51facc2d80c279d40c92890d4c87b854669c
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-25 15:11:41 -0300

    Test for valid access token

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,5 @@
-import time
+import time, json
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 from flask_api_boilerplate.models.user import User
 
@@ -28,3 +29,28 @@ def test_decode_access_token_expired(user):
     result = User.decode_access_token(access_token)
     assert not result.success
     assert result.error == "Access token expired. Login again to renew."
+
+
+def test_decode_acccess_token_invalid(user):
+    access_token = user.encode_access_token()
+    split = access_token.split(".")
+
+    payload_base64 = split[1]
+    pad_len = 4 - (len(payload_base64) % 4)
+
+    payload_base64 += pad_len * "="
+    payload_str = urlsafe_b64decode(payload_base64)
+    payload = json.loads(payload_str)
+
+    assert not payload["admin"]
+
+    payload["admin"] = True
+    payload_mod = json.dumps(payload)
+    payload_mod_base64 = urlsafe_b64encode(payload_mod.encode())
+    split[1] = payload_mod_base64.strip(b"=")
+    access_token_mod = ".".join(str(split))
+    assert not access_token == access_token_mod
+
+    result = User.decode_access_token(access_token_mod)
+    assert not result.success
+    assert result.error == "Invalid token."
