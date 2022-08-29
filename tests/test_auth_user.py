c208a5ca5155f6ecba287b1ef8bdd49fea06e124
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-29 15:11:35 -0300

    Test User Auth

---
+++ tests/test_auth_user.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from flask_api_boilerplate.models.user import User
+from tests.util import EMAIL, PASSWORD, register_user
+
+SUCCESS = "successfully registered"
+
+
+def test_auth_user(client, db):
+    response = register_user(client)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert "status" in response.json and response.json["status"] == "success"
+    assert "message" in response.json and response.json["message"] == SUCCESS
+    assert "token_type" in response.json and response.json["token_type"] == "bearer"
+    assert "expires_in" in response.json and response.json["expires_in"] == 5
+    assert "access_token" in response.json
+
+    access_token = response.json["access_token"]
+    result = User.decode_access_token(access_token)
+    assert result.success
+
+    user_dict = result.value
+    assert not user_dict["admin"]
+
+    user = User.find_by_public_id(user_dict["public_id"])
+    assert user and user.email == EMAIL
