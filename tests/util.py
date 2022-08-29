0b21cae45f96b7a42c9c20b20ed24cb9814cd2b3
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-29 15:04:39 -0300

    Test util for Register User tests

--- tests/util.py
+++ tests/util.py
@@ -1,2 +1,12 @@
+from flask import url_for
+
 EMAIL = "testuser@testmail.com"
 PASSWORD = "teste123"
+
+
+def register_user(test_client, email=EMAIL, password=PASSWORD):
+    return test_client.post(
+        url_for("api.auth_register"),
+        data=f"email={email}&password={password}",
+        content_type="application/x-www-from-urlencoded",
+    )
