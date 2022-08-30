f5d3f1a67f6a921d19e30b8c6f9ec15744074f21
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-30 15:44:41 -0300

    Create widgets class methods and properties

--- src/flask_api_boilerplate/models/widget.py
+++ src/flask_api_boilerplate/models/widget.py
@@ -1,5 +1,15 @@
+from datetime import datetime, timezone, timedelta
+
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from flask_api_boilerplate import db
-from flask_api_boilerplate.utils.datetime import utc_now
+from flask_api_boilerplate.utils.datetime import (
+    utc_now,
+    format_timedelta_str,
+    get_local_utcoffset,
+    localized_dt_string,
+    make_tzaware,
+)
 
 
 class Widget(db.Model):
@@ -18,3 +28,33 @@ class Widget(db.Model):
 
     def __repr__(self):
         return f"<Widget name={self.name}, info_url={self.info_url}"
+
+    @hybrid_property
+    def created_at_str(self):
+        created_at_utc = make_tzaware(
+            self.created_at, use_tz=timezone.utc, localize=False
+        )
+        return localized_dt_string(created_at_utc, use_tz=get_local_utcoffset())
+
+    @hybrid_property
+    def deadline_str(self):
+        deadline_utc = make_tzaware(self.deadline, use_tz=timezone.utc, localize=False)
+        return localized_dt_string(deadline_utc, use_tz=get_local_utcoffset())
+
+    @hybrid_property
+    def deadline_passed(self):
+        return datetime.now(timezone.utc) > self.deadline.replace(tzinfo=timezone.utc)
+
+    @hybrid_property
+    def time_remaining(self):
+        time_remaining = self.deadline.replace(tzinfo=timezone.utc) - utc_now()
+        return time_remaining if not self.deadline_passed else timedelta(0)
+
+    @hybrid_property
+    def time_remaining_str(self):
+        timedelta_str = format_timedelta_str(self.time_remaining)
+        return timedelta_str if not self.deadline_passed else "No time remaining"
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
