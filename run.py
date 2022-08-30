5c62edc969f42616468368d195d13c4eb11f309f
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-30 14:58:18 -0300

    Create add-user cli entry

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os
+import os, click
 
 from flask_api_boilerplate import create_app, db
 from flask_api_boilerplate.models.user import User
@@ -9,3 +9,24 @@ app = create_app(os.getenv("FLASK_ENV", "development"))
 @app.shell_context_processor
 def shell():
     return {"db": db, "User": User}
+
+
+@app.cli.command("add-user", short_help="Add a new user")
+@click.argument("email")
+@click.option(
+    "--admin", is_flag=True, default=False, help="New user has administrator role"
+)
+@click.password_option(help="Do not set password on the command line!")
+def add_user(email, admin, password):
+    """Add a new user to the database with email address = EMAIL."""
+    if User.find_by_email(email):
+        error = f"Error: {email} is already registered"
+        click.secho(f"{error}\n", fg="red", bold=True)
+        return 1
+    new_user = User(email=email, password=password, admin=admin)
+    db.session.add(new_user)
+    db.session.commit()
+    user_type = "admin user" if admin else "user"
+    message = f"Successfully added new {user_type}:\n {new_user}"
+    click.secho(message, fg="blue", bold=True)
+    return 0
