55e95b4cc5fc25cad518d2ace314e3f9a0a37d3a
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-08-31 10:13:25 -0300

    Create Widget DTO -> future date validation

--- src/flask_api_boilerplate/api/widgets/dto.py
+++ src/flask_api_boilerplate/api/widgets/dto.py
@@ -13,7 +13,26 @@ def widget_name(name: str) -> str:
 
     if not re.compile(r"^[\w-]+$").match(name):
         raise ValueError(
-            f"'{name}' contains one or more invalid characters. Widget name must have only letters, number, hyphen or underscore."
+            f"'{name}' contains one or more invalid characters. Widget name must have only letters, number, hyphen or "
+            "underscore. "
         )
 
     return name
+
+
+def future_date_from_string(date_str: str):
+    """Validation method for a date stored as a string."""
+
+    try:
+        parsed_date = parser.parse(date_str)
+    except ValueError:
+        raise ValueError(
+            f"Failed to parse '{date_str}' as a valid date. Make sure to use '2018-5-13' -or- '05/13/2018' -or- 'May "
+            "13 2018'."
+        )
+
+    if parsed_date.date() < date.today():
+        raise ValueError(
+            f"The date {parsed_date.strftime(DATE_MONTH_NAME)} is older than "
+            f"{datetime.now().strftime(DATE_MONTH_NAME)}. You must enter a date in the future. "
+        )
