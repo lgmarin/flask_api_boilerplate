f23da39a406bbea8ae1a25a25171a208a4781835
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-09-01 16:35:35 -0300

    Token blacklisting testing

--- tests/test_auth_logout.py
+++ tests/test_auth_logout.py
@@ -1,22 +1,47 @@
 from http import HTTPStatus
 
 from flask_api_boilerplate.models.token_blacklist import BlacklistedToken
-from tests.util import register_user, login_user, logout_user
+from tests.util import register_user, login_user, logout_user, WWW_AUTH_NO_TOKEN
 
 LOGOUT_SUCCESS = "successfully logged out."
+TOKEN_BLACKLISTED = "Token blacklisted. Please login again."
+WWW_AUTH_BLACKLISTED_TOKEN = (
+    f"{WWW_AUTH_NO_TOKEN}, "
+    'error="invalid_token", '
+    f'error_description="{TOKEN_BLACKLISTED}"'
+)
 
 
 def test_logout(client, db):
     register_user(client)
     response = login_user(client)
     assert "access_token" in response.json
+
     access_token = response.json["access_token"]
     blacklist = BlacklistedToken.query.all()
     assert len(blacklist) == 0
+
     response = logout_user(client, access_token)
     assert response.status_code == HTTPStatus.OK
     assert "status" in response.json and response.json["status"] == "success"
     assert "message" in response.json and response.json["message"] == LOGOUT_SUCCESS
+
     blacklist = BlacklistedToken.query.all()
     assert len(blacklist) == 1
     assert access_token == blacklist[0].token
+
+
+def test_logout_token_blacklisted(client, db):
+    register_user(client)
+    response = login_user(client)
+    assert "access_token" in response.json
+
+    access_token = response.json["access_token"]
+    response = logout_user(client, access_token)
+    assert response.status_code == HTTPStatus.OK
+
+    response = logout_user(client, access_token)
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert "message" in response.json and response.json["message"] == TOKEN_BLACKLISTED
+    assert "WWW-Authenticate" in response.headers
+    assert response.headers["WWW-Authenticate"] == WWW_AUTH_BLACKLISTED_TOKEN
