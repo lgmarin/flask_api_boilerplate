498a8003d8771e0d04f33be53c34f042407bbcab
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-09-05 14:56:12 -0300

    Add Pagination DTO Models

--- src/flask_api_boilerplate/api/widgets/dto.py
+++ src/flask_api_boilerplate/api/widgets/dto.py
@@ -2,6 +2,8 @@ import re
 from datetime import date, datetime, timezone, time
 
 from dateutil import parser
+from flask_restx import Model
+from flask_restx.fields import Boolean, DateTime, Integer, List, Nested, String, Url
 from flask_restx.inputs import URL, positive
 from flask_restx.reqparse import RequestParser
 
@@ -43,7 +45,7 @@ def future_date_from_string(date_str: str):
     return deadline_utc
 
 
-# Parser
+# Request Parser
 widget_req_parser = RequestParser(bundle_errors=True)
 widget_req_parser.add_argument(
     "name",
@@ -75,4 +77,48 @@ pagination_req_parser = RequestParser(bundle_errors=True)
 pagination_req_parser.add_argument("page", type=positive, required=False, default=1)
 pagination_req_parser.add_argument(
     "per_page", type=positive, required=False, default=10, choices=[5, 10, 25, 50]
+)
+
+
+# Flask Restx Model
+widget_owner_model = Model("Widget Owner", {"email": String, "public_id": String})
+
+widget_model = Model(
+    "Widget",
+    {
+        "name": String,
+        "info_url": String,
+        "created_at_iso8601": DateTime(attribute="created_at"),
+        "created_at_rfc822": DateTime(attribute="created_at", dt_format="rfc822"),
+        "deadline": String(attribute="deadline_str"),
+        "deadline_passed": Boolean,
+        "time_remaining": String(attribute="time_remaining_str"),
+        "owner": Nested(widget_owner_model),
+        "link": Url("api.widget"),
+    },
+)
+
+pagination_links_model = Model(
+    "Nav Links",
+    {
+        "self": String,
+        "prev": String,
+        "next": String,
+        "first": String,
+        "last": String,
+    },
+)
+
+pagination_model = Model(
+    "Pagination",
+    {
+        "links": Nested(pagination_links_model, skip_none=True),
+        "has_prev": Boolean,
+        "has_next": Boolean,
+        "page": Integer,
+        "total_pages": Integer(attribute="pages"),
+        "items_per_page": Integer(attribute="per_page"),
+        "total_items": Integer(attribute="total"),
+        "items": List(Nested(widget_model)),
+    },
 )
